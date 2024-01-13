89279b5d5e1e22b97cd7b41b0ed4ddc6270ab19b
FrankWu <frankwu2029@gmail.com>
2024-01-13 21:13:39 +0800

    update

--- step2_get_list.py
+++ step2_get_list.py
@@ -3,99 +3,46 @@ import os
 from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import os
+import glob
 
 if __name__ == '__main__':
 
-    data_path = './data/'
+    # /content/3D_teeth_semi_segment/data/data_part_1/upper/EKITH3BB
+    data_path = '/content/3D_teeth_semi_segment/data/*/*/*'
     output_path = './'
-    num_augmentations = 20
     train_size = 0.8
-    with_flip = True
 
-    path_temp = "E:/teeth_3D/3DTeethSeg22_challenge/teeth_3D_data_center_trans/teeth/"
-    file_lists = os.listdir(path_temp)
-    print(file_lists)
+    file_lists = glob.glob(data_path)
+    # for file in file_lists[:300]:
+    #     print(file)
+    # /content/3D_teeth_semi_segment/data/data_part_1/upper/EKITH3BB
+    # /content/3D_teeth_semi_segment/data/data_part_1/lower/O52P1SZT/O52P1SZT_lower.json
 
-    sample_list = np.asarray(sample_list)
+    sample_list = np.asarray(file_lists)
+    arr_tmp = []
+    for sample in sample_list:
+        sample_tmp = sample.split('/')
+        sample_ = sample + "/" + sample_tmp[-1] + "_" + sample_tmp[-2]+".obj"
+        arr_tmp.append(sample_)
 
-
-    train_list, val_list = train_test_split(sample_list, train_size=0.8, shuffle=True)
+    train_list, val_list = train_test_split(arr_tmp, train_size=0.8, shuffle=True)
     val_list, test_list = train_test_split(val_list, train_size=0.5, shuffle=True)
     print('Training list:\n', train_list, '\nValidation list:\n', val_list, '\nTest list:\n', test_list)
 
-    #training
-    train_name_list = []
-    for i_sample in train_list:
-        #print('Computing Sample: {0}; Aug: {1}...'.format(i_sample, i_aug))
-        subject_name = 'A{}_Sample_0{}_d.vtp'.format(i_aug, i_sample)
-        train_name_list.append(os.path.join(data_path, subject_name))
-        if with_flip:
-            subject2_name = 'A{}_Sample_0{}_d.vtp'.format(i_aug, i_sample+1000)
-            train_name_list.append(os.path.join(data_path, subject2_name))
-
-    with open(os.path.join(output_path, 'train_list_{0}.csv'.format(i_cv)), 'w') as file:
-        for f in train_name_list:
+    with open(os.path.join(output_path, 'train_list_{0}.csv'.format(1)), 'w') as file:
+        for f in train_list:
+            file.write(f+'\n')
+    with open(os.path.join(output_path, 'val_list_{0}.csv'.format(1)), 'w') as file:
+        for f in val_list:
+            file.write(f+'\n')
+    with open(os.path.join(output_path, 'test_list_{0}.csv'.format(1)), 'w') as file:
+        for f in test_list:
             file.write(f+'\n')
 
-
     print('--------------------------------------------')
-    print('with flipped samples:', with_flip)
-    print('# of train:', len(train_name_list))
-    print('# of validation:', len(val_name_list))
+    print('# of train:', len(train_list))
+    print('# of validation:', len(val_list))
+    print('# of test:', len(test_list))
     print('--------------------------------------------')
 
-
-    #print(sample_list)
-
-    i_cv = 0
-    kf = KFold(n_splits=6, shuffle=False)
-    for train_idx, test_idx in kf.split(sample_list):
-
-        i_cv += 1
-        print('Round:', i_cv)
-
-        train_list, test_list = sample_list[train_idx], sample_list[test_idx]
-        train_list, val_list = train_test_split(train_list, train_size=0.8, shuffle=True)
-
-        print('Training list:\n', train_list, '\nValidation list:\n', val_list, '\nTest list:\n', test_list)
-
-        #training
-        train_name_list = []
-        for i_sample in train_list:
-            for i_aug in range(num_augmentations):
-                #print('Computing Sample: {0}; Aug: {1}...'.format(i_sample, i_aug))
-                subject_name = 'A{}_Sample_0{}_d.vtp'.format(i_aug, i_sample)
-                train_name_list.append(os.path.join(data_path, subject_name))
-                if with_flip:
-                    subject2_name = 'A{}_Sample_0{}_d.vtp'.format(i_aug, i_sample+1000)
-                    train_name_list.append(os.path.join(data_path, subject2_name))
-
-        with open(os.path.join(output_path, 'train_list_{0}.csv'.format(i_cv)), 'w') as file:
-            for f in train_name_list:
-                file.write(f+'\n')
-
-        #validation
-        val_name_list = []
-        for i_sample in val_list:
-            for i_aug in range(num_augmentations):
-                #print('Computing Sample: {0}; Aug: {1}...'.format(i_sample, i_aug))
-                subject_name = 'A{}_Sample_0{}_d.vtp'.format(i_aug, i_sample)
-                val_name_list.append(os.path.join(data_path, subject_name))
-                if with_flip:
-                    subject2_name = 'A{}_Sample_0{}_d.vtp'.format(i_aug, i_sample+1000)
-                    val_name_list.append(os.path.join(data_path, subject2_name))
-
-        with open(os.path.join(output_path, 'val_list_{0}.csv'.format(i_cv)), 'w') as file:
-            for f in val_name_list:
-                file.write(f+'\n')
-
-        #test
-        test_df = pd.DataFrame(data=test_list, columns=['Test ID'])
-        test_df.to_csv('test_list_{}.csv'.format(i_cv), index=False)
-
-
-        print('--------------------------------------------')
-        print('with flipped samples:', with_flip)
-        print('# of train:', len(train_name_list))
-        print('# of validation:', len(val_name_list))
-        print('--------------------------------------------')
