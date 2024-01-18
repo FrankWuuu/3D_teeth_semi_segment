190cf1dca719476959c3f251296d7bad5dafa820
FrankWu <frankwu2029@gmail.com>
2024-01-18 14:14:19 +0800

    Update plot_fig.py

--- plot_fig.py
+++ plot_fig.py
@@ -5,19 +5,14 @@ from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import StepLR
 import torch.optim as optim
 import torch.nn as nn
-# from Mesh_dataset import *
-# from meshsegnet import *
+
 from losses_and_metrics_for_mesh import *
-import utils
 import pandas as pd
-import shutil
+# import shutil
 import csv
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-
-    use_visdom = True # if you don't use visdom, please set to False
-
 
     # ,loss,DSC,SEN,PPV,val_loss,val_DSC,val_SEN,val_PPV
     path = "losses_metrics_vs_epoch.csv"
@@ -37,8 +32,7 @@ if __name__ == '__main__':
     val_DSC =dating_df["val_DSC"].values
     val_SEN =dating_df["val_SEN"].values
     val_PPV =dating_df["val_PPV"].values
-    # print(len(index))
-    # print(len(loss))
+
     plt.plot(index,loss,index,val_loss)
     plt.xlabel("epoches")
     plt.ylabel("loss")
